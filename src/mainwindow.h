de03b8506f2d7c1022ad3c0417df99b3925fc2a4
aztecian batman <aztecianbatman@gmail.com>
2026-09-05 22:11:06 +0530

    Expand editor window controls for advanced core tools

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,65 +2,22 @@
 #include <QMainWindow>
 #include <QString>
 
-class QLabel;
-class QListWidget;
-class QSlider;
-class QSpinBox;
-class QTreeWidget;
-class QTimer;
-class QAction;
-class FluxCanvas;
-class FluxWheel;
-class FluxDocument;
+class QLabel; class QListWidget; class QSlider; class QSpinBox; class QTreeWidget; class QTimer; class QAction;
+class FluxCanvas; class FluxDocument;
 
 class FluxMainWindow final : public QMainWindow {
     Q_OBJECT
 public:
-    explicit FluxMainWindow(QWidget* parent = nullptr);
-    ~FluxMainWindow() override;
-
+    explicit FluxMainWindow(QWidget* parent=nullptr); ~FluxMainWindow() override;
 private slots:
-    void newProject();
-    void openProject();
-    void saveProject();
-    void saveProjectAs();
-    void exportImage();
-    void addLayer();
-    void duplicateLayer();
-    void removeLayer();
-    void layerSelectionChanged();
-    void setFrameFromTimeline(int row);
-    void togglePlayback();
-    void autosave();
-    void updateBrushSize(int value);
-    void chooseColor();
-    void setToolFromAction();
+    void newProject(); void openProject(); void saveProject(); void saveProjectAs(); void exportImage();
+    void addLayer(); void duplicateLayer(); void removeLayer(); void layerSelectionChanged(); void setFrameFromTimeline(int row); void togglePlayback(); void autosave();
+    void updateBrushSize(int value); void chooseColor(); void setToolFromAction(); void openBrushEditor();
+    void mirrorHorizontal(); void mirrorVertical(); void rotateCanvas(); void fitCanvas(); void toggleOnionSkin(); void selectAll(); void deselect(); void undo(); void redo();
     void setStatus(const QString& text);
-
 private:
-    void buildMenus();
-    void buildToolbar();
-    void buildDocks();
-    QWidget* makeLayersPanel();
-    QWidget* makeInspectorPanel();
-    QWidget* makeTimelinePanel();
-    void refreshLayers();
-    void refreshTimeline();
-    void restoreLastSession();
-    void markModified();
-    void polish();
-
-    FluxDocument* m_document{};
-    FluxCanvas* m_canvas{};
-    QLabel* m_statusLabel{};
-    QLabel* m_brushSizeLabel{};
-    QLabel* m_cursorLabel{};
-    QSlider* m_brushSlider{};
-    QTreeWidget* m_layers{};
-    QListWidget* m_frames{};
-    QSpinBox* m_fps{};
-    QTimer* m_playTimer{};
-    bool m_playing=false;
-    QString m_filePath;
-    QString m_autosavePath;
+    void buildMenus(); void buildToolbar(); void buildDocks(); QWidget* makeLayersPanel(); QWidget* makeInspectorPanel(); QWidget* makeTimelinePanel();
+    void refreshLayers(); void refreshTimeline(); void restoreLastSession(); void markModified(); void polish();
+    FluxDocument* m_document{}; FluxCanvas* m_canvas{}; QLabel* m_statusLabel{}; QLabel* m_brushSizeLabel{}; QLabel* m_cursorLabel{}; QLabel* m_colorSwatch{};
+    QSlider* m_brushSlider{}; QTreeWidget* m_layers{}; QListWidget* m_frames{}; QSpinBox* m_fps{}; QTimer* m_playTimer{}; bool m_playing=false; QString m_filePath; QString m_autosavePath;
 };

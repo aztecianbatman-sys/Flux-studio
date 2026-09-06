ae393032ae56ac105f8bba2f22f6cd81649c64c7
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 10:23:13 +0530

    Upgrade compositor panel to an interactive node graph

--- src/fluxcompositorwidget.h
+++ src/fluxcompositorwidget.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <QWidget>
+#include <QHash>
 #include "fluxcompositor.h"
-class QListWidget; class QComboBox; class QDoubleSpinBox; class QPushButton;
+
+class QComboBox; class QDoubleSpinBox; class QPushButton; class QGraphicsView; class QGraphicsScene; class QGraphicsItem; class QGraphicsLineItem;
 class FluxCompositorWidget final : public QWidget {
     Q_OBJECT
 public:
@@ -10,6 +12,8 @@ public:
 private slots:
     void addNode(); void removeNode(); void renderPreview(); void syncSelection(); void parameterChanged();
 private:
-    void rebuild(); void showNodeParameters();
-    FluxCompositor m_compositor; QListWidget* m_nodes{}; QComboBox* m_type{}; QDoubleSpinBox* m_value1{}; QDoubleSpinBox* m_value2{}; QDoubleSpinBox* m_value3{}; QPushButton* m_preview{};
+    void rebuild(); void showNodeParameters(); void rebuildGraph(); void createDefaultGraph();
+    FluxCompositor m_compositor;
+    QGraphicsView* m_view{}; QGraphicsScene* m_scene{}; QComboBox* m_type{}; QDoubleSpinBox* m_value1{}; QDoubleSpinBox* m_value2{}; QDoubleSpinBox* m_value3{}; QPushButton* m_preview{};
+    QHash<int,QPointF> m_positions; QHash<int,QGraphicsItem*> m_items;
 };

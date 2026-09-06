d880b72398101d5f434d481a241fbd2c0e95fba7
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 13:53:31 +0530

    Add interactive Flux color wheel widget

---
+++ src/fluxcolorwheel.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <QColor>
+#include <QWidget>
+
+class FluxColorWheel final : public QWidget {
+    Q_OBJECT
+public:
+    explicit FluxColorWheel(QWidget* parent=nullptr);
+    QColor color() const { return m_color; }
+    void setColor(const QColor& color);
+signals:
+    void colorChanged(const QColor& color);
+protected:
+    void paintEvent(QPaintEvent*) override;
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
+private:
+    void pick(const QPointF& point);
+    QColor m_color{42, 128, 255};
+    bool m_dragging=false;
+};

81ff9dff836f6fc1940dadee35cfcb37f63e7a5c
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 07:51:20 +0530

    Align app version with Flux Studio 0.6

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("Flux Studio");
-    QCoreApplication::setApplicationVersion("0.1.0");
+    QCoreApplication::setApplicationVersion("0.6.0");
     QCoreApplication::setOrganizationName("Flux");
 
     FluxSplash splash;

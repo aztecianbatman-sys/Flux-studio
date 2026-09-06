a066a1176aacddd5e394f8d04ac7b2a686c7b45c
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 12:31:41 +0530

    Upgrade render queue widget status and cancellation controls

--- src/fluxrenderqueuewidget.h
+++ src/fluxrenderqueuewidget.h
@@ -2,15 +2,20 @@
 #include <QWidget>
 #include "fluxrenderqueue.h"
 
-class QListWidget; class QProgressBar; class QPushButton;
+class QListWidget; class QProgressBar; class QPushButton; class QLabel;
 class FluxRenderQueueWidget final : public QWidget {
     Q_OBJECT
 public:
     explicit FluxRenderQueueWidget(QWidget* parent=nullptr);
     FluxRenderQueue& queue(){return m_queue;}
+    const FluxRenderQueue& queue() const{return m_queue;}
     void addJob(const FluxRenderJob& job);
+    void refreshQueue();
+    void setRunning(bool running);
+    void setProgress(double progress, const QString& status);
 signals:
     void renderRequested();
+    void cancelRequested();
 private slots:
     void removeSelected();
     void clearQueue();
@@ -22,4 +27,7 @@ private:
     FluxRenderQueue m_queue;
     QListWidget* m_list{};
     QPushButton* m_render{};
+    QPushButton* m_cancel{};
+    QProgressBar* m_progress{};
+    QLabel* m_status{};
 };

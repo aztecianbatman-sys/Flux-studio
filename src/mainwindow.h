34160065115c1008747c1078f495b8ad7b2fd1ce
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 07:04:57 +0530

    Expand layer workspace controls

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,7 +2,7 @@
 #include <QMainWindow>
 #include <QString>
 
-class QLabel; class QListWidget; class QSlider; class QSpinBox; class QTreeWidget; class QTimer;
+class QLabel; class QListWidget; class QSlider; class QSpinBox; class QTreeWidget; class QTimer; class QComboBox;
 class FluxCanvas; class FluxDocument;
 
 class FluxMainWindow final : public QMainWindow {
@@ -11,14 +11,19 @@ public:
     explicit FluxMainWindow(QWidget* parent=nullptr); ~FluxMainWindow() override;
 private slots:
     void newProject(); void openProject(); void saveProject(); void saveProjectAs(); void exportImage();
-    void addLayer(); void duplicateLayer(); void removeLayer(); void layerSelectionChanged(); void setFrameFromTimeline(int row); void togglePlayback(); void autosave();
+    void addLayer(); void addGroup(); void addMask(); void duplicateLayer(); void removeLayer(); void mergeDown(); void flattenVisible();
+    void layerSelectionChanged(); void layerItemChanged(QTreeWidgetItem* item, int column); void setLayerOpacity(int value); void setLayerBlendMode(int index);
+    void setLayerLocked(bool enabled); void setLayerClipping(bool enabled); void setLayerAlphaInheritance(bool enabled); void setLayerSolo(bool enabled); void setLayerIsolate(bool enabled); void setLayerLabelColor(); void setLayerStyle();
+    void syncLayerTreeToDocument();
+    void setFrameFromTimeline(int row); void togglePlayback(); void autosave();
     void updateBrushSize(int value); void chooseColor(); void setToolFromAction(); void openBrushEditor();
     void mirrorHorizontal(); void mirrorVertical(); void rotateCanvas(); void fitCanvas(); void toggleOnionSkin(); void selectAll(); void deselect(); void undo(); void redo();
     void setStatus(const QString& text);
 private:
     void buildMenus(); void buildToolbar(); void buildDocks(); QWidget* makeLayersPanel(); QWidget* makeInspectorPanel(); QWidget* makeTimelinePanel();
-    void refreshLayers(); void refreshTimeline(); void restoreLastSession(); void markModified(); void polish();
+    void refreshLayers(); void addLayerTreeItem(QTreeWidgetItem* parent, int index); void refreshTimeline(); void restoreLastSession(); void markModified(); void polish();
+    FluxMainWindow* rootWindow();
     FluxDocument* m_document{}; FluxCanvas* m_canvas{}; QLabel* m_statusLabel{}; QLabel* m_brushSizeLabel{}; QLabel* m_cursorLabel{}; QLabel* m_colorSwatch{};
-    QSlider* m_brushSlider{}; QTreeWidget* m_layers{}; QListWidget* m_frames{}; QSpinBox* m_fps{}; QTimer* m_playTimer{}; bool m_playing=false;
-    bool m_mirrorH=false; bool m_mirrorV=false; qreal m_canvasRotation=0; QString m_filePath; QString m_autosavePath;
+    QSlider* m_brushSlider{}; QTreeWidget* m_layers{}; QListWidget* m_frames{}; QSpinBox* m_fps{}; QTimer* m_playTimer{}; QComboBox* m_blendMode{};
+    bool m_playing=false; bool m_syncingLayers=false; bool m_mirrorH=false; bool m_mirrorV=false; qreal m_canvasRotation=0; QString m_filePath; QString m_autosavePath;
 };

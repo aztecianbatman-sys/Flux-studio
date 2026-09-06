567daba0b3fbd153b28de6177eb3d82f94b57a61
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 07:22:05 +0530

    Expose timeline frame state for playback integration

--- src/fluxtimelinewidget.h
+++ src/fluxtimelinewidget.h
@@ -7,6 +7,8 @@ class FluxTimelineWidget final : public QWidget {
 public:
     explicit FluxTimelineWidget(FluxDocument*document,QWidget*parent=nullptr);
     FluxAnimationSystem& animation(){return m_animation;}
+    void setCurrentFrame(int frame);
+    int currentFrame() const{return m_current;}
 signals:
     void frameChanged(int frame);
     void documentEdited();
@@ -17,5 +19,5 @@ protected:
     void keyPressEvent(QKeyEvent*)override;
 private:
     void addKeyAt(int frame); void duplicateFrame(); void insertFrame(); void deleteFrame();
-    FluxDocument* m_document{}; FluxAnimationSystem m_animation; int m_current=0; int m_rowHeight=28; int m_frameWidth=18; int m_trackOffset=42; bool m_dope=true; bool m_graph=false;
+    FluxDocument* m_document{}; FluxAnimationSystem m_animation; int m_current=0; int m_rowHeight=28; int m_frameWidth=18; int m_trackOffset=110; bool m_graph=false;
 };

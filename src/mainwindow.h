443af810bd04c8bd3cc841feb583b70b999fb219
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 09:39:32 +0530

    Rebuild Flux Studio workstation shell

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,8 +5,8 @@
 #include <QSpinBox>
 #include <QString>
 
-class QLabel; class QSlider; class QTreeWidget; class QTimer; class QComboBox; class QTreeWidgetItem;
-class FluxCanvas; class FluxDocument;
+class QLabel; class QSlider; class QTreeWidget; class QTimer; class QComboBox; class QTreeWidgetItem; class QStackedWidget; class QWidget; class QAction; class QToolBar;
+class FluxCanvas; class FluxDocument; class FluxWheel;
 
 class FluxMainWindow final : public QMainWindow {
     Q_OBJECT
@@ -22,10 +22,12 @@ private slots:
     void updateBrushSize(int value); void chooseColor(); void setToolFromAction(); void openBrushEditor();
     void mirrorHorizontal(); void mirrorVertical(); void rotateCanvas(); void fitCanvas(); void toggleOnionSkin(); void selectAll(); void deselect(); void undo(); void redo();
     void setStatus(const QString& text);
+    void enterWorkspace(); void returnHome(); void openRecentProject(); void updateZoomLabel(double zoom); void showCommandPalette();
 private:
-    void buildMenus(); void buildToolbar(); void buildDocks(); QWidget* makeLayersPanel(); QWidget* makeInspectorPanel(); QWidget* makeTimelinePanel();
+    void buildHome(); void buildMenus(); void buildTopBar(); void buildToolRail(); void buildDocks(); QWidget* makeLayersPanel(); QWidget* makeInspectorPanel(); QWidget* makeTimelinePanel();
     void refreshLayers(); void addLayerTreeItem(QTreeWidgetItem* parent, int index); void refreshTimeline(); void restoreLastSession(); void markModified(); void polish();
-    FluxDocument* m_document{}; FluxCanvas* m_canvas{}; QLabel* m_statusLabel{}; QLabel* m_brushSizeLabel{}; QLabel* m_cursorLabel{}; QLabel* m_colorSwatch{};
-    QSlider* m_brushSlider{}; QTreeWidget* m_layers{}; QListWidget* m_frames{}; QSpinBox* m_fps{}; QTimer* m_playTimer{}; QComboBox* m_blendMode{};
+    void loadProjectPath(const QString& path); void rememberRecent(const QString& path); void updateHomeRecent();
+    FluxDocument* m_document{}; FluxCanvas* m_canvas{}; QWidget* m_home{}; QStackedWidget* m_stack{}; QLabel* m_statusLabel{}; QLabel* m_brushSizeLabel{}; QLabel* m_cursorLabel{}; QLabel* m_colorSwatch{}; QLabel* m_zoomLabel{};
+    QSlider* m_brushSlider{}; QTreeWidget* m_layers{}; QListWidget* m_frames{}; QSpinBox* m_fps{}; QTimer* m_playTimer{}; QComboBox* m_blendMode{}; FluxWheel* m_wheel{}; QToolBar* m_topBar{}; QToolBar* m_toolRail{};
     bool m_playing=false; bool m_syncingLayers=false; bool m_mirrorH=false; bool m_mirrorV=false; qreal m_canvasRotation=0; QString m_filePath; QString m_autosavePath;
 };

94e617689ed826925365a634a70593da085bd3d9
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 07:43:53 +0530

    Add Flux Wheel command and hover interaction API

--- src/fluxwheel.h
+++ src/fluxwheel.h
@@ -1,16 +1,22 @@
 #pragma once
 #include <QWidget>
+#include <QPoint>
 
+class QMouseEvent;
 class FluxWheel final : public QWidget {
     Q_OBJECT
 public:
     explicit FluxWheel(QWidget* parent=nullptr);
 public slots:
     void openAt(const QPoint& globalPos);
+signals:
+    void commandTriggered(int index);
 protected:
     void paintEvent(QPaintEvent*) override;
     void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
 private:
     QPoint m_center;
     bool m_open=false;
+    int m_hover=-1;
 };

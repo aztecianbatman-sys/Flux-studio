cc74ea4b14758665b2f6e814de011745e42fba68
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 10:21:53 +0530

    Cache one composite per canvas frame instead of recomputing per tile

--- src/fluxcanvasengine.h
+++ src/fluxcanvasengine.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <QImage>
 #include <QPointF>
 #include <QRectF>
@@ -7,47 +6,21 @@
 
 class QPainter;
 class FluxDocument;
-class BrushEngine;
 
 class FluxCanvasEngine final {
 public:
-    static constexpr int TileSize = 256;
-
-    void setDocument(FluxDocument* document);
-    FluxDocument* document() const { return m_document; }
-
-    void setZoom(qreal zoom);
-    qreal zoom() const { return m_zoom; }
-    void setRotation(qreal degrees);
-    qreal rotation() const { return m_rotation; }
-    void setMirror(bool horizontal, bool vertical = false);
-    bool mirrorHorizontal() const { return m_mirrorH; }
-    bool mirrorVertical() const { return m_mirrorV; }
-    void panBy(const QPointF& delta);
-    QPointF pan() const { return m_pan; }
-    void resetView();
-    void fitToViewport(const QSize& viewport);
-
-    QPointF widgetToCanvas(const QPointF& point, const QSize& viewport) const;
-    QPointF canvasToWidget(const QPointF& point, const QSize& viewport) const;
-    QRectF visibleCanvasRect(const QSize& viewport) const;
-
-    void draw(QPainter& painter, const QSize& viewport);
-    void invalidate();
-    void setPixelPerfect(bool enabled) { m_pixelPerfect = enabled; }
-    bool pixelPerfect() const { return m_pixelPerfect; }
-
+    static constexpr int TileSize=256;
+    void setDocument(FluxDocument*document); FluxDocument* document()const{return m_document;}
+    void setZoom(qreal zoom); qreal zoom()const{return m_zoom;}
+    void setRotation(qreal degrees); qreal rotation()const{return m_rotation;}
+    void setMirror(bool horizontal,bool vertical=false); bool mirrorHorizontal()const{return m_mirrorH;} bool mirrorVertical()const{return m_mirrorV;}
+    void panBy(const QPointF&delta); QPointF pan()const{return m_pan;} void resetView(); void fitToViewport(const QSize&viewport);
+    QPointF widgetToCanvas(const QPointF&point,const QSize&viewport)const; QPointF canvasToWidget(const QPointF&point,const QSize&viewport)const; QRectF visibleCanvasRect(const QSize&viewport)const;
+    void draw(QPainter&painter,const QSize&viewport); void invalidate();
+    void setPixelPerfect(bool enabled){m_pixelPerfect=enabled;} bool pixelPerfect()const{return m_pixelPerfect;}
 private:
-    struct Tile { QPoint grid; QImage image; bool valid=false; };
-    QImage tileFor(const QPoint& grid);
-    QImage renderTile(const QPoint& grid) const;
-
-    FluxDocument* m_document{};
-    QVector<Tile> m_tiles;
-    qreal m_zoom=1.0;
-    qreal m_rotation=0.0;
-    QPointF m_pan;
-    bool m_mirrorH=false;
-    bool m_mirrorV=false;
-    bool m_pixelPerfect=false;
+    struct Tile{QPoint grid;QImage image;bool valid=false;};
+    QImage tileFor(const QPoint&grid); QImage renderTile(const QImage&composite,const QPoint&grid)const;
+    FluxDocument* m_document{}; QVector<Tile> m_tiles; QImage m_cachedComposite; bool m_compositeValid=false;
+    qreal m_zoom=1.0; qreal m_rotation=0.0; QPointF m_pan; bool m_mirrorH=false,m_mirrorV=false,m_pixelPerfect=false;
 };

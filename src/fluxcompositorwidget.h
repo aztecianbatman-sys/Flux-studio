49cf14f7939d2cb7e2caed3ae365710453b561b8
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 12:33:04 +0530

    Prepare compositor UI for persistent graph state and navigation

--- src/fluxcompositorwidget.h
+++ src/fluxcompositorwidget.h
@@ -12,7 +12,7 @@ public:
 private slots:
     void addNode(); void removeNode(); void renderPreview(); void syncSelection(); void parameterChanged();
 private:
-    void rebuild(); void showNodeParameters(); void rebuildGraph(); void createDefaultGraph();
+    void rebuild(); void showNodeParameters(); void rebuildGraph(); void createDefaultGraph(); void saveGraphState(); void loadGraphState();
     FluxCompositor m_compositor;
     QGraphicsView* m_view{}; QGraphicsScene* m_scene{}; QComboBox* m_type{}; QDoubleSpinBox* m_value1{}; QDoubleSpinBox* m_value2{}; QDoubleSpinBox* m_value3{}; QPushButton* m_preview{};
     QHash<int,QPointF> m_positions; QHash<int,QGraphicsItem*> m_items;

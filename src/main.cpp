1821edf29c221c19cf75c19c4af99b7a98a08b8d
aztecian batman <aztecianbatman@gmail.com>
2026-09-06 15:13:31 +0530

    Launch Flux Studio preview workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <QApplication>
 #include <QIcon>
-#include "mainwindow.h"
-
+#include "fluxstudio_nextwindow.h"
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("Flux Studio");
     QCoreApplication::setApplicationVersion("0.9.0");
     QCoreApplication::setOrganizationName("Flux");
     app.setWindowIcon(QIcon(QStringLiteral(":/branding/flux-logo.svg")));
-
-    FluxMainWindow window;
+    FluxNextWindow window;
     window.show();
     return app.exec();
 }
